a9b07bbea6026f176c85474e6478085db73483e6
xGinko <rachel.audrey@mail.de>
2025-01-12 23:40:52 +0100

    rework pipe smoke module

--- src/main/java/ron/thewizard/roleplayextras/modules/PipeSmoke.java
+++ src/main/java/ron/thewizard/roleplayextras/modules/PipeSmoke.java
@@ -4,20 +4,30 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.ItemMergeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PipeSmoke extends RoleplayExtrasModule implements Listener {
 
+    private final Material pipeMaterial;
     private final Particle particleType;
-    private final double offsetX, offsetY, offsetZ, speed, headDistance;
+    private final double offsetY, speed, headDistance;
     private final int particleCount;
 
     public PipeSmoke() {
-        super("gameplay.pipe-smoke-particles", true);
+        super("gameplay.pipe-smoke-particles", true, """
+                Right-clicking with the configured item will place smoke particles in front
+                of the players face.""");
+        Material defaultPipe = Material.GOAT_HORN;
+        Material configuredPipe;
+        try {
+            configuredPipe = Material.valueOf(config.getString(configPath + ".pipe-material", defaultPipe.name()));
+        } catch (IllegalArgumentException e) {
+            configuredPipe = defaultPipe;
+        }
+        this.pipeMaterial = configuredPipe;
+
         Particle defaultParticle = Particle.CAMPFIRE_COSY_SMOKE;
         Particle configuredParticle;
         try {
@@ -26,12 +36,13 @@ public class PipeSmoke extends RoleplayExtrasModule implements Listener {
             configuredParticle = defaultParticle;
         }
         this.particleType = configuredParticle;
-        this.particleCount = config.getInt(configPath + ".particle-count", 20);
-        this.offsetX = config.getDouble(configPath + ".offset-x", 0);
+
+        this.particleCount = config.getInt(configPath + ".particle-count", 6);
         this.offsetY = config.getDouble(configPath + ".offset-y", 0);
-        this.offsetZ = config.getDouble(configPath + ".offset-z", 0);
-        this.speed = config.getDouble(configPath + ".speed", 0.05);
-        this.headDistance = config.getDouble(configPath + ".head-distance", 0.5);
+        this.speed = config.getDouble(configPath + ".speed", 0.01);
+        this.headDistance = config.getDouble(configPath + ".head-distance", 0.5, """
+                A positive value will put the smoke in front of the player's head.
+                A negative value will put it behind the player's head.""");
     }
 
     @Override
@@ -44,23 +55,19 @@ public class PipeSmoke extends RoleplayExtrasModule implements Listener {
         HandlerList.unregisterAll(this);
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    private void on(ItemMergeEvent event) {
-        event.setCancelled(true);
-    }
-
     @EventHandler
-    public void on(PlayerInteractEvent event) {
-        if (event.getMaterial() != Material.GOAT_HORN) return;
+    private void on(PlayerInteractEvent event) {
+        if (event.getAction().isLeftClick() || event.getMaterial() != pipeMaterial) return;
 
-        Location location = event.getPlayer().getEyeLocation();
+        final Location eyeLocation = event.getPlayer().getEyeLocation().clone();
 
         event.getPlayer().getWorld().spawnParticle(
                 particleType,
-                location.add(location.getDirection().multiply(headDistance)),
+                eyeLocation.add(eyeLocation.getDirection().multiply(headDistance)),
                 particleCount,
-                offsetX, offsetY, offsetZ,
-                speed
-        );
+                0,
+                offsetY,
+                0,
+                speed);
     }
 }
